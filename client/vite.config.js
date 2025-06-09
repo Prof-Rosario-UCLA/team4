5c8e0a73885c83836ac76eec2cc4dd17b5974681
Jason Lin <lin1jason8@outlook.com>
2025-06-09 13:37:54 -0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -36,7 +36,7 @@ export default defineConfig({
           },
           {
             src: "pwa-512x512.png",
-            sizes: "512x512",c
+            sizes: "512x512",
             type: "image/png",
             purpose: "any maskable"
           },
